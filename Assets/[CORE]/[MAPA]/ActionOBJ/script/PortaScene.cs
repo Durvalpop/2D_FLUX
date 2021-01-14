ded5aa0dc7664408102b466844efb7a653c3163b
Durvalpop <durval.jr1999@hotmail.com>
2021-01-14 17:25:13 -0300

    UP

---
+++ Assets/[CORE]/[MAPA]/ActionOBJ/script/PortaScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortaScene : MonoBehaviour
+{
+    public GameObject PlayerObj;
+    public string scenaName;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player");
+            SceneManager.LoadScene(scenaName);
+            DontDestroyOnLoad(PlayerObj);
+        }
+    }
+}
